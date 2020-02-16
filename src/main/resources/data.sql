608a40bdb3059b6fa7d16b16fdf4863ef20446f3
Clinton Morrison <morrison.clinton@gmail.com>
2020-02-16 09:12:45 -0500

    Reformat

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO student (registration, password, firstName, lastName, email) VALUES
+('2091245678', 'password', 'John', 'Doe', 'johndoe@gmail.com'),
+('7895612349', 'xyz1234', 'Jane', 'Doe', 'janedoe@yahoo.com');
+
+
+INSERT INTO subject (name, description) VALUES
+('Mathematics', 'Teaching and learning how to solve problems using algorithms and formulas necessary for computations.'),
+('English', 'Analyzing short stories and  novels including nonfiction, drama, poems, and myths'),
+('Physics', 'The concepts of vectors and scalars using the kinematic equations, graphs of motion, and reference frames and relative motions');
